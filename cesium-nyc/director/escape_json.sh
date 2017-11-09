a66a8784be5fbea698d772af3f372a8e3da3fbe6
Bryan Berry <bryan@endpoint.com>
2017-11-09 11:16:04 -0800

    czml: Updated uscs_message to not escape quotes

--- cesium-nyc/director/escape_json.sh
+++ cesium-nyc/director/escape_json.sh
@@ -4,5 +4,5 @@
 rm *_escaped.json
 # Run modification operations
 for n in *.json; do 
-  echo "        \"uscs_message\": \"$(cat $n | tr -d '\n' | sed -e 's/\ //g' | sed -e 's/\"/\\\"/g')\"," > ${n%.json}_escaped.json;
+  echo "        \"uscs_message\": \"$(cat $n | tr -d '\n' | sed -e 's/\ //g')\"," > ${n%.json}_escaped.json;
 done
