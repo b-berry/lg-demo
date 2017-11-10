b662f1e911e40fa871e0f097bba22d6e270b566e
Bryan Berry <bryan@endpoint.com>
2017-11-10 12:30:45 -0800

    czml: Renamed director files to accomidate hostnames

--- cesium-nyc/director/escape_json.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Remove previous snippets
-rm *_escaped.json
-# Run modification operations
-for n in *.json; do 
-  echo "        \"uscs_message\": \"$(cat $n | tr -d '\n' | sed -e 's/\ //g')\"," > ${n%.json}_escaped.json;
-done
